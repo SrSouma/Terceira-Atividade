62c84f02d66f09ca7f938fe284efef1e37f71ce8
valmir junior <101368262+SrSouma@users.noreply.github.com>
2022-03-18 00:06:57 +0000

    change

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ def unifran ():
 @app.route('/dashboard/<nome>')
 def dashboard(nome):
     return f'Olá {nome}'
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
